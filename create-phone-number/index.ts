1ec9dfe6373975dabdf4bb8efd1a21429127e0ed
G_Anta <ganta@telecomboutique.com>
2022-02-14 11:00:40 +0100

    Revert "first 7 solutions"
    This reverts commit 411cb9d305f395b035dfb33852aa16eedae16ebe.

--- create-phone-number/index.ts
+++
@@ -1,5 +0,0 @@
-function createPhoneNumber(numbers: number[]): string {
-    let numberString = numbers.join("");
-    let phoneNumber = `(${numberString.slice(0,3)}) ${numberString.slice(3,6)}-${numberString.slice(6)}`;
-    return phoneNumber;
-}
--- get-middle-character/index.ts
+++
@@ -1,8 +0,0 @@
-export class Challenge {
-    static getMiddle(s: string) {
-        const stringLength = s.length;
-        const isOdd = stringLength % 2 === 0;
-        const middle = isOdd ? stringLength / 2 : Math.floor(stringLength / 2);
-        return isOdd ? s.slice(middle - 1, middle + 1) : s.slice(middle, middle + 1);
-    }
-}
