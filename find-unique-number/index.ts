aefafafe7719b01a91e2a1a8df3b1de24321742c
G_Anta <ganta@telecomboutique.com>
2022-02-15 10:35:48 +0100

    find-unique-number && tsconfig

---
+++ find-unique-number/index.ts
@@ -0,0 +1,11 @@
+console.log(findUniq([1, 1, 1, 2, 1, 1]));
+function findUniq(arr: number[]): number {
+    const map: { [key: number]: number } = {};
+    for (let index = 0; index < arr.length; index++) {
+        map[arr[index]] = arr[index] in map
+            ? map[arr[index]] + 1
+            : 1;
+    }
+    const unique = Object.entries(map).find(([, count]) => count === 1);
+    return unique ? parseFloat(unique[0]) : 0;
+}
